2195bf7286f53467535dff39f3ba795f3c1db707
crystal-valdez <crystaljjgao@gmail.com>
2024-04-22 15:10:33 -0400

    removing other api endpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import { ChatWindow } from "@/components/ChatWindow";
+import ChatInterface from "./livewell_link/page";
 
 export default function Home() {
-  const InfoCard = (
-    <div className="p-0 md:p-0 rounded text-black w-full max-h-[85%] overflow-hidden flex flex-col justify-center items-center">
-      <h1 className="font-velino text-xl text-center">
-        livewell link
-      </h1>
-    </div>
-  );
-
   return (
-  <div>
-    <ChatWindow
-      endpoint="api/chat"
-      emoji="🩺"
-      titleText="Livewell Link"
-      placeholder="How is your health today?"
-      emptyStateComponent={InfoCard}
-    ></ChatWindow>
-  </div>
+    <div>
+      <ChatInterface/>
+    </div>
   );
 }
